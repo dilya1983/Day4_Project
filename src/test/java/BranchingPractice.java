f7ba46229779e7924aa0ec795af5c56fb6405f9d
dilya1983 <dkhamidova1@gmail.com>
2021-05-25 19:30:10 -0400

    second commit

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -5,6 +5,7 @@ public class BranchingPractice {
         // enabled version control
         // added .gitegnore with .idea , target folder and .iml file
         // reviewing on new branch
+        // doing more review
 
     }
 }
