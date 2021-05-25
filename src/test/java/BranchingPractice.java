b380ec3f76e53a8eab1a77219a3f873c2573edfe
dilya1983 <dkhamidova1@gmail.com>
2021-05-25 19:23:40 -0400

    thinks done

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,8 @@
 public class BranchingPractice {
     public static void main(String[] args) {
 
+        // created new project
+        // enabled version control
+        // added .gitegnore with .idea , target folder and .iml file
     }
 }
