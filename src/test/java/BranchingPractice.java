9f8a8b6013c4b9cc09e7daee139ab1d3543f68ec
dilya1983 <dkhamidova1@gmail.com>
2021-05-25 20:23:56 -0400

    added changes

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -4,6 +4,7 @@ public class BranchingPractice {
         // created new project
         // enabled version control
         // added .gitegnore with .idea , target folder and .iml file
+
         // reviewing on new branch
         // doing more review
 
